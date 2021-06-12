7321100ac7c41b25fb8bdc591c40f385c049b1d8
CJBuchel <connorbuchel@gmail.com>
2021-06-13 03:14:17 +0800

    GLFW buildable added to project dir as submodule and in premake

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 Small game engine for low end visual games, mostly a side project. (AKA Wrapper lib currently for OpenGL designed imported as a DLL/Static Lib for game projects) in VS
 
 
-
-
 ## Setup for building
 
 1. Build setup
---
+++ config.lua
@@ -0,0 +1,4 @@
+ROOT = "./" -- This directory style works on all platforms, not just unix
+SolDir = ROOT .. "Solution/" -- Directory for sultion files and project files for VS
+VendorDir = ROOT .. "vendors/"
+OutputDir = ROOT .. "bin/" -- Directory for binary output
