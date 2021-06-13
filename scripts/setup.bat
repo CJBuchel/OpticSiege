1fc6304a5b10eecb8d853da090fb272faffa89ec
CJBuchel <connorbuchel@gmail.com>
2021-06-13 21:35:30 +0800

    Added... literally nothing

---
+++ scripts/setup.bat
@@ -0,0 +1,5 @@
+:: Setup using windows
+
+echo "Setting up project..."
+echo "yea... do it yourself. Just set the environment variable for path to point to Premake\bin\premake5.exe batch files are a pain and i just deleted all my system variables trying to automate it..."
+:: setx premake5 "%cd%..\Premake\bin\premake5.exe" /M
